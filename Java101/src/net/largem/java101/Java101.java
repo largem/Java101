18aa93d927b3b9dda21e169447e2b46e0a4b7a77
HD <HD@192.168.2.13>
2016-04-02 17:15:20 -0400

    Hello World

--- Java101/src/net/largem/java101/Java101.java
+++ Java101/src/net/largem/java101/Java101.java
@@ -3,8 +3,8 @@ package net.largem.java101;
 public class Java101 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//the classic start point
+		System.out.println("Hello World");
 	}
 
 }
