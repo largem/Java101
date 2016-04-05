81bc0dfc2eda9c2dc0dd2c2ddb0bd52258383a86
HD <HD@192.168.2.13>
2016-04-04 23:02:26 -0400

    make common class not to be instantiate

--- Java101/src/net/largem/common/Common.java
+++ Java101/src/net/largem/common/Common.java
@@ -4,4 +4,8 @@ public final class Common {
 	public static void logMe(Class<?> c) {
 		System.out.println("Running in " + c.getSimpleName());
 	}
+	
+	private Common(){
+		throw new AssertionError();
+	}
 }
