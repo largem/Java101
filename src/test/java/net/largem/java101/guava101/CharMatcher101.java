b7be8623136aa5cc4a20cfef424eb60f5c9bd066
James Tan <largemtan@gmail.com>
2016-11-14 23:36:15 -0500

    stream list to map

--- src/test/java/net/largem/java101/guava101/CharMatcher101.java
+++ src/test/java/net/largem/java101/guava101/CharMatcher101.java
@@ -1,25 +1,34 @@
 package net.largem.java101.guava101;
 
 import com.google.common.base.CharMatcher;
-import com.google.common.base.Charsets;
 import org.junit.Test;
 
-import javax.print.DocFlavor;
-import java.util.Arrays;
-import java.util.stream.Stream;
-
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by James Tan on 11/10/2016.
  */
-public final class CharMatcher101 {
 
-    private final static String[] STRINGS_WITH_SPACE={"AB C", "AB\tC", "AB\nC"};
+/**
+ * CharMatcher works like a predicate to match char.
+ * There are set of predefined charMatcher and a couple of operations.
+  */
+public final class CharMatcher101 {
 
     @Test
     public void example_BREAKING_WHITESPACE()
     {
-        final long c = Arrays.stream(STRINGS_WITH_SPACE).filter(s -> CharMatcher.breakingWhitespace().matchesAllOf("ABC"))
-                            .count();
+        // \u00a0 is non-breaking whitespace
+        final String stringWithWhiteSpace = "AB C, AB\tC, AB\nC, 100\u00a0km";
+
+        // whitespace() matches both breaking and non-breaking whitespace.
+        final String removeWhiteSpace = CharMatcher.whitespace().removeFrom(stringWithWhiteSpace);
+        //final String retainWhiteSpace = CharMatcher.whitespace().retainFrom(stringWithWhiteSpace);
+
+        // breakingWhitespace does not match non-breaking whitespace.
+        final String removeBreakingWhitespace = CharMatcher.breakingWhitespace().removeFrom(stringWithWhiteSpace);
+        assertThat(removeWhiteSpace, is(not(removeBreakingWhitespace)));
     }
 }
