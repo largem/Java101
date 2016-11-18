63a0bdeab1ce16c2e6c4fa43148fea1c73b8f6e5
James Tan <largemtan@gmail.com>
2016-11-17 22:53:08 -0500

    abc

---
+++ src/main/java/net/largem/java101/guava101/Objects101.java
@@ -0,0 +1,15 @@
+package net.largem.java101.guava101;
+
+/**
+ * Created by James Tan on 11/15/2016.
+ */
+
+/**
+ * Objects provides a couple of helper funtion to make toStrng, hashcode, comparable easily.
+ */
+
+public class Objects101 {
+    public static void main(String[] args) {
+
+    }
+}
